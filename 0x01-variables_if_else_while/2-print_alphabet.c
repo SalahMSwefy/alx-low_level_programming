f9ea9e904115f222d174f8135773dccb8f058f7b
Salah Swefy <salahm.swefy@gmail.com>
2023-07-15 01:44:28 +0300

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	printf("\n");
+	return (0);
+}

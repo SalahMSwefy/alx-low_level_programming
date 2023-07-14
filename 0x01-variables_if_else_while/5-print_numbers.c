5d42f3370d0551fb78e20dbd08d4f818ecf008f2
Salah Swefy <salahm.swefy@gmail.com>
2023-07-15 01:55:49 +0300

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+
+		printf("%d", c);
+	}
+
+	printf("\n");
+	return (0);
+}

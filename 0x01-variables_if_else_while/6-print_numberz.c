5513fa019a3de47709986613956e53bec3d65e97
Salah Swefy <salahm.swefy@gmail.com>
2023-07-15 02:00:12 +0300

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+
+		putchar(c);
+	}
+
+	printf("\n");
+	return (0);
+}

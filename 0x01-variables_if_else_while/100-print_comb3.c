b0496918c5989a39cb07402d96deae03ee2b301b
Salah Swefy <salahm.swefy@gmail.com>
2023-07-15 04:57:36 +0300

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int c, a;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			if (a <= c)
+				continue;
+			putchar(c + '0');
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

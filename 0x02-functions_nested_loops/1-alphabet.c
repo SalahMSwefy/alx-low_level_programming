719b6bea236c844888ec9ab177f07482dbd849f3
Salah Swefy <salahm.swefy@gmail.com>
2023-07-18 19:21:36 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * function for print alphabet
- */
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
 void print_alphabet(void)
 {
 	int i;

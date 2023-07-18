6b327c33ab83ade76887473ab7ed64e315e8414a
Salah Swefy <salahm.swefy@gmail.com>
2023-07-18 19:25:35 +0300

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet - function that print alphabet
+ *it prints in lowercase
+ *Return: no return
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+
+	for (j = 0; j < 11; j++)
+	{
+
+		int i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}

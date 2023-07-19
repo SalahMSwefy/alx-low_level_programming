7b2e65d53d3c80feb86441f655d41bf816f3832e
Salah Swefy <salahm.swefy@gmail.com>
2023-07-19 19:10:11 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,37 +6,39 @@
 void print_times_table(int n)
 {
 	int i, j;
-
-	for (i = 0; i <= n; i++)
+	if (n > 0 && n < 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-
-			int frist = (i * j) / 100;
-			int sec = frist % 10;
-			int last = (i * j) % 10;
-
-			if (frist)
-				_putchar(frist + '0');
-			else
+			for (j = 0; j <= n; j++)
 			{
-				if (j != 0)
-				_putchar(' ');
-			}
-			if (!frist && !sec)
-			{
+
+				int frist = (i * j) / 100;
+				int sec = ((i * j) % 100) / 10;
+				int last = (i * j) % 10;
+
+				if (frist)
+					_putchar(frist + '0');
+				else
+				{
 					if (j != 0)
+						_putchar(' ');
+				}
+				if (!frist && !sec)
+				{
+					if (j != 0)
+						_putchar(' ');
+				}
+				else
+					_putchar(sec + '0');
+				_putchar(last + '0');
+				if (j != n)
+				{
+					_putchar(',');
 					_putchar(' ');
+				}
 			}
-			else
-				_putchar(sec + '0');
-			    _putchar(last + '0');
-			if (j != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }

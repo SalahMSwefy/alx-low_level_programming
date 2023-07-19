cbf12eaaedf7d54e2d46ee54403fde734e7806e6
Salah Swefy <salahm.swefy@gmail.com>
2023-07-19 17:25:28 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,23 @@
 #include "main.h"
 /**
- *jack_bauer - a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *jack_bauer - a function that prints every minute of the day of Jack Bauer,
+  starting from 00:00 to 23:59
  *Return: returns 0
  */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 60; j++)
 		{
-			for (k = 0; k < 6; k++)
-			{
-				for (l = 0; l < 10; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }

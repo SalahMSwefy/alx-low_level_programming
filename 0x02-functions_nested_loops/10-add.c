1333ac10efb182b4b977bac010b80aa172e520bb
Salah Swefy <salahm.swefy@gmail.com>
2023-07-19 18:08:37 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,27 @@
 #include "main.h"
 /**
- *add -  a  function that adds two integers and returns the result.
- *@a: is a number
- *@b: is a number
+ *print_to_98 -  a function that prints the numbers from n to 98.
  *Return: no return
  */
-int add(int a, int b)
+void print_to_98(int n)
 {
-	return (a + b);
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
 }

5b7e9e046a3c7f427c3de8c12e3ad1460f361a08
Salah Swefy <salahm.swefy@gmail.com>
2023-07-19 19:50:49 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *natural - prints the sum of all the multiples of 3 or 5 below 1024,
+ * mian - prints the sum of all the multiples of 3 or 5 below 1024,
  *followed by a new line.
  *Return: no return
  */

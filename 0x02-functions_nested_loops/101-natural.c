735ae7da63f75739b31452a89fd7b3f3375b7576
Salah Swefy <salahm.swefy@gmail.com>
2023-07-19 19:50:01 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 	int i, x;
+	
 	x = 0;
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			x += i;
 	}
-	printf("%d\n",x);
+	printf("%d\n", x);
 	return (0);
 }

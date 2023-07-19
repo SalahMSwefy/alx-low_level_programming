8b1065fa5e5a80d6ede7ffc075cfb1a632a00169
Salah Swefy <salahm.swefy@gmail.com>
2023-07-19 17:50:23 +0300

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,16 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if ((i * j) / 10)
+			if (i != 0)
 			{
-				_putchar(((i * j) / 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
+				if ((i * j) / 10)
+				{
+					_putchar(((i * j) / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
 			}
 			_putchar(((i * j) % 10) + '0');
 			if (j != 9)

a09a9789e93495a5e8b212a6d22133878c45c4cb
Salah Swefy <salahm.swefy@gmail.com>
2023-07-20 16:37:49 +0300

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,18 +8,16 @@
 
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar(n % 10 + '0');
-		return;
-	}
+	unsigned int num = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar(n % 10 + '0');
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
 }
+

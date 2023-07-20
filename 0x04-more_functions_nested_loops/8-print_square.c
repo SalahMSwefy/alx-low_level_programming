bc9de2ac1376bc386ae2cb00cdf197129273d80a
Salah Swefy <salahm.swefy@gmail.com>
2023-07-20 15:14:15 +0300

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_diagonal - prints a square
+ *@n:parameter
+ *Return:returns nothing
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}

aae4daba070c33b8672f87af09b65a5e07fd7b5e
Salah Swefy <salahm.swefy@gmail.com>
2023-07-20 14:01:05 +0300

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_diagonal - prints a diagonal line
+ *@n:parameter
+ *Return:returns nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+}

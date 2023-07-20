17be59c994379e3abf88276ffde33c60af50d770
Salah Swefy <salahm.swefy@gmail.com>
2023-07-20 12:55:35 +0300

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,11 @@
  */
 void print_numbers(void)
 {
-	int i;
-
-	for (i = 48; i <= 57; i++)
-	{
-		_putchar(i);
-	}
-	_putchar(10);
+int i = 48;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar(10);
 }
 

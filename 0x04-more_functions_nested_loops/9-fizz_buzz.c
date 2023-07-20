250fa70da293fe845e09c55ab294e18b9f8a5d3b
Salah Swefy <salahm.swefy@gmail.com>
2023-07-20 15:32:46 +0300

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,14 @@
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
 		else
 			printf("%d ", i);
 	}
-	return (0);
+	printf("\n");
 }

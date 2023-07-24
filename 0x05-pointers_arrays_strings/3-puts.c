6a86451ee5ddba8f9af52f3571663f4eb2413c7e
Salah Swefy <salahm.swefy@gmail.com>
2023-07-24 14:30:22 +0300

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen -a function that print a string.
+ *@s: is a string
+ *Return: return
+ */
+void _puts(char *str);
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		_putchar(s[i]);
+	}
+
+}

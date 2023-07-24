849e030c23c6c38fc5fd085fd48a2ac78bb2d409
Salah Swefy <salahm.swefy@gmail.com>
2023-07-24 14:40:53 +0300

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_rev -a function that prints a string, in reverse
+ *@s: is a string
+ *Return: no return
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

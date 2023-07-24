39f3f0c305ab964624aef70a9de5c132d46f7aaa
Salah Swefy <salahm.swefy@gmail.com>
2023-07-24 14:05:06 +0300

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
  /**
- *reset_to_98 -a function that updates the value it points to to 98.
+ *reset_to_98 -a function that updates the value 
+ *of the parameter to to 98.
  *@n: is a parameter
  *Return: return nothing
  */

02520496dfe77523ca6be1a14b1b0260d0c3463e
Salah Swefy <salahm.swefy@gmail.com>
2023-07-26 18:29:01 +0300

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ **_strncpy -a function that copies the string pointed
+ *@dest: is a string
+ *@src: is a string
+ *Return: no return
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != 0 && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (n > i)
+		dest[i] = 0;
+
+	return (dest);
+}

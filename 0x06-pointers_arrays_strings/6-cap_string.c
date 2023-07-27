fe7469c718b7e1096b386c5cffb809962c780563
Salah Swefy <salahm.swefy@gmail.com>
2023-07-27 17:49:41 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,24 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 0, j, dif = 0;
+
+	char chars[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', '\0'};
 
 	if (s[i] >= 'a' && s[i] <= 'z')
 		s[i] = s[i] - 32;
 
 	for (i = 1; s[i] != 0; i++)
 	{
-		if (!((s[i - 1] >= 'a' && s[i - 1] <= 'z') || (s[i - 1] >= 'A' && s[i - 1] <= 'Z') || (s[i - 1] >= '0' && s[i - 1] <= '9')))
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - dif;
+		for (j = 0; j < 14; j++)
 		{
-			if (s[i] >= 97 && s[i] <= 122)
-				s[i] = s[i] - 32;
+			if (s[i] == chars[j])
+			{
+				dif = 32;
+				break;
+			}
 		}
 	}
 

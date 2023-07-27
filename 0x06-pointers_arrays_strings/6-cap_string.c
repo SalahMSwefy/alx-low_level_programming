b7eed1807c41be08091751db0e84d94f63714204
Salah Swefy <salahm.swefy@gmail.com>
2023-07-27 17:38:45 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string - a function that capitalizes all words of a string.
  * @s: This is the input string
- * Return: Lowercase changed to uppercase of the string
+ * Return: capitalized string
  */
 
 char *cap_string(char *s)
@@ -15,7 +15,7 @@ char *cap_string(char *s)
 
 	for (i = 1; s[i] != 0; i++)
 	{
-		if (!((s[i-1] >= 'a' && s[i-1] <= 'z') || (s[i-1] >= 'A' && s[i-1] <= 'Z')))
+		if (!((s[i - 1] >= 'a' && s[i - 1] <= 'z') || (s[i - 1] >= 'A' && s[i - 1] <= 'Z') || (s[i - 1] >= '0' && s[i - 1] <= '9')))
 		{
 			if (s[i] >= 97 && s[i] <= 122)
 				s[i] = s[i] - 32;

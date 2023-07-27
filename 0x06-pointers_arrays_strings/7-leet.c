e877391877c351d5292ac3ee3978b37bf6a6cc6b
Salah Swefy <salahm.swefy@gmail.com>
2023-07-27 18:33:46 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,28 +3,28 @@
 /**
  * leet - a function that encodes a string into 1337.
  * @s: This is the input string
- * Return: capitalized string
+ * Return: string
  */
 
 char *leet(char *s)
 {
-	int i = 0;
-	char c;
+    int i = 0, j;
+    char c;
 
-	char chars[] = {'A', 'E', 'O', 'T', 'L', '4', '3', '0', '7', '1'};
+    char chars[] = {'A', 'E', 'O', 'T', 'L', '4', '3', '0', '7', '1'};
 
-	for (i = 0; s[i] != 0; i++)
-	{
-		c = s[i] - dif;
+    for (i = 0; s[i] != 0; i++)
+    {
+        c = s[i] - 32;
 
-		for (j = 0; j < 5; j++)
-		{
-			if (c == chars[j])
-			{
-				s[i] = chars[j + 5];
-			}
-		}
-	}
+        for (j = 0; j < 5; j++)
+        {
+            if (c == chars[j])
+            {
+                s[i] = chars[j + 5];
+            }
+        }
+    }
 
-	return (s);
+    return (s);
 }

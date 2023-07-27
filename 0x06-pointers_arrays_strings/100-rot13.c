af406d3b11bcf930f98bf1ffb4ee9da20f2873b7
Salah Swefy <salahm.swefy@gmail.com>
2023-07-27 19:08:43 +0300

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string into rot13.
+ * @s: This is the input string
+ * Return: string encoded to rot13
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char real[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; j < 53; j++)
+		{
+			if (s[i] == real[j])
+			{
+				s[i] = rot[j];
+			}
+		}
+	}
+
+	return (s);
+}

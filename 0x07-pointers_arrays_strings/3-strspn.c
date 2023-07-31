b37f4cadf4d79222e592efd68c116d9b137dc7a9
Salah Swefy <salahm.swefy@gmail.com>
2023-07-31 22:00:08 +0300

    more_pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,24 +9,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, cnt;
+    unsigned int i, j, cnt;
 
-	cnt = 0;
-	for (i = 0; s[i] != 0; i++)
-	{
-		for (j = 0; accept[i] != 0;; j++)
-		{
-			if (s[i] == accept[i])
-			{
-				cnt++;
-				break;
-			}
-		}
-		if (s[i] = !accept[i])
-		{
-			break;
-		}
-	}
+    cnt = 0;
+    for (i = 0; s[i] != 0; i++)
+    {
+        for (j = 0; accept[j] != 0; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                cnt++;
+                break;
+            }
+        }
+        if (s[i] != accept[j])
+        {
+            break;
+        }
+    }
 
-	return (cnt);
+    return (cnt);
 }

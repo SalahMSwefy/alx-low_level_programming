6d29dcb8bec8fc881e02cc7573ca78a9408c8b34
Salah Swefy <salahm.swefy@gmail.com>
2023-07-31 22:54:38 +0300

    more_pointers_arrays_strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,17 +6,17 @@
  *Return: nothing
  */
 
-void print_chessboard(char(*a)[8])
+void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j;
+    unsigned int i, j;
 
-	for (i = 0; a[i] != 0; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            putchar(a[i][j]);
+        }
 
-		_putchar(10);
-	}
+        putchar(10);
+    }
 }

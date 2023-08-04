53f1337e8c75a5e10a315166f0e5ef4ab803ebd5
Salah Swefy <salahm.swefy@gmail.com>
2023-08-04 03:32:18 +0300

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int _prime(int n, int i);
+
+/**
+ *_pow_recursion - a function that returns 1 if the input integer is
+ *a prime number, otherwise return 0.
+ *@n: the input number
+ *Return: 1 if the input integer is a prime number, otherwise return 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+		return (0);
+	return (_prime(n, n - 1));
+}
+
+/**
+ *_prime - a function that returns if a number is prime
+ *@n: the input number
+ *@i: iterator
+ *Return: 1 if the input integer is a prime number, otherwise return 0.
+ */
+
+int _prime(int n, int i)
+{
+	if (n == 1)
+		return (1);
+	if (n > 0 && n % i == 0)
+	{
+		return (0);
+	}
+	return (_sqrt(n, i - 1));
+}

e11191fa298988b4641a090f579771f02d0a9443
Salah Swefy <salahm.swefy@gmail.com>
2023-08-08 20:07:37 +0300

    malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
-			for (i; i >= 0; i--)
+			for (i--; i >= 0; i--)
 				free(arr[i]);
 
 			free(arr);

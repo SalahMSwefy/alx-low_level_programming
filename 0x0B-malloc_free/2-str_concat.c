dbb5a503a42a037aa3388c9974c3e2fbab4efec8
Salah Swefy <salahm.swefy@gmail.com>
2023-08-08 19:10:04 +0300

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, size1, size2;
 	char *arr;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	size1 = size2 = 0;

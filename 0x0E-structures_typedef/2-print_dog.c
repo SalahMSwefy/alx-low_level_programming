3c68be6b76485b22d63bcb4a7599b2c5bf4205d2
salahswefy <salahm.swefy@gmail.com>
2023-08-15 03:37:48 +0300

    structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ *print_dog -  a function that prints a struct dog
+ *@d: dog information
+ *Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+	if
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}

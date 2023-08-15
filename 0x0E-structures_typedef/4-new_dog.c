0fbf77640725b50c1a573ded07a58968d528ad74
salahswefy <salahm.swefy@gmail.com>
2023-08-15 05:23:40 +0300

    structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,50 +2,37 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
+ *_strlen -a function that return the length of a string.
+ *@s: is a string
+ *Return: return ength of a string.
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	while (s[i] != 0)
 	{
 		i++;
 	}
 
 	return (i);
 }
-
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
+ **_strcpy -a function that copies the string pointed
+ *@dest: is a string
+ *@src: is a string
+ *Return: return a coped string.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int i = 0;
 
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
+	while (src[i] != 0)
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	dest[i] = '\0';
+	dest[i] = 0;
 
 	return (dest);
 }

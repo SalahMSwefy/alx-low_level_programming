1294ccf11f4cb0fea34a1bb5843be9450de0317b
salahswefy <salahm.swefy@gmail.com>
2023-08-15 04:26:49 +0300

    structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,17 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-	if
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		if ((*d).name != NULL)
+			printf("Name: %s\n", (*d).name);
+		else
+			printf("nil\n");
+		if ((*d).age > 0)
+			printf("Age: %s\n", (*d).age);
+		else
+			printf("nil\n");
+		if ((*d).owner != NULL)
+			printf("Owner: %s\n", (*d).owner);
+		else
+			printf("nil\n");
 	}
 }

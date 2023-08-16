bc90fa5f04b4bcea809b2cf39e0f1321b067cdce
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-16 18:26:32 +0300

    function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,13 +13,11 @@
 
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
-	int i = 0;
+	int i;
 
 	if (array == NULL || action == NULL)
 		return;
-	while (i < size)
-	{
+		
+	for (i = 0; i < size; i++)
 		action(array[i]);
-		i++;
-	}
 }

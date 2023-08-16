b942fb3dc510c56b3bd4a2342779b20cdc22c2fb
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-16 19:55:28 +0300

    function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,9 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, res;
-	int (*func)(int, int);
-	char *oop;
+	int one, two, ans;
+	int (*res)(int, int);
+	char *get_op;
 
 	if (argc != 4)
 	{
@@ -21,24 +21,25 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	sign = argv[2];
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
 
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != 0)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((oop == '/' || oop == '%') && (argv[3] == 0))
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	func = get_op_func(oop);
-	res = func(a, b);
-	printf("%d\n", res);
+	res = get_op_func(get_op);
+	ans = res(one, two);
+
+	printf("%d\n", ans);
 	return (0);
 }

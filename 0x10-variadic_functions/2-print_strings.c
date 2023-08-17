ce6748bb6e349d70f3c11c3dac9f85ebc21cb738
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-17 18:04:17 +0300

    variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (separator == NULL || *separator == 0)
 		s = "";
 	else
-		s = (char*) separator;
+		s = (char *) separator;
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{

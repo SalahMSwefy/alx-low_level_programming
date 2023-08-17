84a4a9b802eeb9b40fd3494c3db675661382d09e
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-17 17:24:10 +0300

    variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list num;
 
 	va_start(num, n);
-	if (separator == NULL)
+	if (separator == NULL || *separator == 0)
 		separator = "";
 
 	for (i = 0; i < n; i++)

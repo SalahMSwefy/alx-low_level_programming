74d2872a3fe78358c51c1958a9d035d34847c8e0
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-29 19:26:50 +0300

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,8 +17,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	x->n = n;
-	x->next = *head;
+	x->next = NULL;
 	*head = x;
-
+    
 	return (x);
 }
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ *add_nodeint_end -a function that adds a new node
+ *at the end of a listint_t list.
+ *@n: integer
+ *@head: a pointer to the beginning of a list_t list.
+ *Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *x, *temp;
+
+	x = malloc(sizeof(listint_t));
+	if (x == NULL)
+		return (NULL);
+
+	x->n = n;
+	x->next = NULL;
+	*head = x;
+
+	if (*head == NULL)
+		*head = x;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = x;
+	}
+
+	return (x);
+}

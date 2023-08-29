abdb8a1deaddae619e5a889d3c0a7cf18d232b9f
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-29 19:08:39 +0300

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- *print_listint - function that prints all
+ *listint_len - function that prints all
  *the elements of a listint_t list.
  *@h: listint_t - to print it
  *Return: the number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	const listint_t *start = h;
 	size_t cnt = 0;
 
 	while (start != NULL)
 	{
-		printf("%d\n", start->n);
 		cnt++;
 		start = start->next;
 	}

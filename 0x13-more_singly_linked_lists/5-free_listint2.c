0568c89a600dc1ed7f03ed5c33abbfae4f59d7b6
SalahMSwefy <salahm.swefy@gmail.com>
2023-08-29 20:15:13 +0300

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (head != NULL)
@@ -19,4 +19,6 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
+
+	head = NULL;
 }

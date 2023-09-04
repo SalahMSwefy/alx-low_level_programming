c3feaa861ce30c179c6e66bf09ab0774a420f8c8
SalahMSwefy <salahm.swefy@gmail.com>
2023-09-04 19:43:19 +0300

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[i] != NULL)
-		i++;
+	if (text_content != NULL)
+	{
+		while (text_content[i] != NULL)
+			i++;
+	}
 
 	ofile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wfile = write(ofile, text_content, i);

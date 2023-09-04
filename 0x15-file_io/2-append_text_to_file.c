2ae239e4529c1f74c830e5f52b9ce71ba4b98957
SalahMSwefy <salahm.swefy@gmail.com>
2023-09-04 19:39:30 +0300

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- *create_file - a function that creates a file.
+ *append_text_to_file - a function that reads a text file
+ *and prints it to the POSIX standard output.
  *@filename: a pointer to the name of file
- *@text_content: string to write to the file
+ *@text_content: a string to read and print
  *Return: the actual number of letters it could read and print
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int ofile, wfile, i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[i] != NULL)
+	while (text_content != NULL)
 		i++;
 
 	ofile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);

3ca16a0a06835a5c5b63c8232c36de8844fb9557
SalahMSwefy <salahm.swefy@gmali.com>
2023-11-04 00:47:22 +0200

    # 0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	temp = ht->array[index];
 	while (temp)
